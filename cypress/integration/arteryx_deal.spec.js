cb0abf37a9c4992b826da8f24fe7bb14e2ffe4b1
Jason Le <le.kent.jason@gmail.com>
2021-08-05 13:55:31 -0700

    Delete arteryx check

--- cypress/integration/arteryx_deal.spec.js
+++
@@ -1,8 +0,0 @@
-context("Arteryx", () => {
-    it("RFD Deal Scan", () => {
-        cy.visit("https://forums.redflagdeals.com/search.php?keywords=arcteryx&terms=all&tags_terms=all&author=&fid%5B%5D=9&sc=1&sf=titleonly&sr=topics&ch=300&t=0&submit=Search");
-
-        cy.get('.no_results')
-            .should('exist');
-    })
-})
